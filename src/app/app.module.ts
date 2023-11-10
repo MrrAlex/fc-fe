74460f334526e2f43fb1f09ca85f59acc369a8c0
Aleks Zavyalov <zavyalov.aleks@gmail.com>
2023-11-10 11:09:28 +0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -70,7 +70,7 @@ const routes: Routes = [
     {
       provide: APP_INITIALIZER,
       useFactory: () => {
-        window.postMessage(true);
+        return window.postMessage(true);
       },
       multi: true
     },
